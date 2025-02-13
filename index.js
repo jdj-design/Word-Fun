e80956547b2fc5b870e37be59259afd324a08843
jdj-design <doc_jones12@yahoo.com>
2025-02-13 18:48:04 -0500

    adding in first even listener

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log();
+// Event listener for clicking the flower area
+document.getElementById('tomato').addEventListener('click', function(){
+    fetch('https://api.dictionaryapi.dev/api/v2/entries/en/tomato')
+    .then(rawResponse => rawResponse.json())
+    .then(jsonifiedResponse => console.log(jsonifiedResponse))
+})
