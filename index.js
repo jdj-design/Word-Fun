0547a4f94570ad4065abd2667f59acd291f2a330
jdj-design <doc_jones12@yahoo.com>
2025-02-14 14:17:14 -0500

    got the call to work

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-// Event listener for clicking the flower area
-console.log('connection sucessful');
-document.getElementById('flowerArea').addEventListener('click', function(){
-    fetch('https://api.dictionaryapi.dev/api/v2/entries/en/tomato')
-    .then(rawResponse => rawResponse.json())
-    .then(jsonifiedResponse => console.log(jsonifiedResponse))
-});
+console.log('connection successful');
 
+// document.getElementById('flowerArea').addEventListener('click', function(event) {
+//     event.preventDefault(); 
+
+    fetch('https://api.dictionaryapi.dev/api/v2/entries/en/flower')
+        .then(rawResponse => rawResponse.json())
+        .then(jsonifiedResponse => {
+            //console.log(jsonifiedResponse);
+
+           
+            const definition = jsonifiedResponse[0].meanings[0].definitions[1].definition;
+            console.log(definition);
+        })
+        .catch(error => console.error('Error fetching data:', error));
+//});
