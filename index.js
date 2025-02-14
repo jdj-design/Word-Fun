a2b7eeea7156b3563a8315dedaf2a5a796ef90a2
jdj-design <doc_jones12@yahoo.com>
2025-02-14 11:28:09 -0500

    update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Event listener for clicking the flower area
-document.getElementById('tomato').addEventListener('click', function(){
+console.log('connection sucessful');
+document.getElementById('flowerArea').addEventListener('click', function(){
     fetch('https://api.dictionaryapi.dev/api/v2/entries/en/tomato')
     .then(rawResponse => rawResponse.json())
     .then(jsonifiedResponse => console.log(jsonifiedResponse))
-})
+});
+
