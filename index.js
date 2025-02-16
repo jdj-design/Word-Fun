eebed2fe15ad21a57c43444b3fb6a311b9b75a04
jdj-design <doc_jones12@yahoo.com>
2025-02-15 19:36:51 -0500

    got the leaf to show

--- index.js
+++ index.js
@@ -11,7 +11,24 @@ document.addEventListener('DOMContentLoaded', function() {
 
                 const definition = jsonifiedResponse[0].meanings[0].definitions[1].definition;
                 console.log(definition);
-                document.getElementById('definitionText').innerText = `Definition: ${definition}`;
+                document.getElementById('definitionText').innerText = `Flower: ${definition}`;
+            })
+            .catch(error => console.error('Error fetching data:', error));
+    });
+
+
+
+    document.getElementById('leafs').addEventListener('click', function(event) {
+        // event.preventDefault();  // Uncomment this if needed to prevent the default action
+
+        fetch('https://api.dictionaryapi.dev/api/v2/entries/en/leaf')
+            .then(rawResponse => rawResponse.json())
+            .then(jsonifiedResponse => {
+                console.log(jsonifiedResponse);
+
+                const leafDef = jsonifiedResponse[0].meanings[0].definitions[0].definition;
+                console.log(leafDef);
+                document.getElementById('leafDefinition').innerText = `Leaf: ${leafDef}`;
             })
             .catch(error => console.error('Error fetching data:', error));
     });
